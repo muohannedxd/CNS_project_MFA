af52e96407f87dbcac0b8c27d64cab9f527f2686
muohannedxd <mohannedx1105@gmail.com>
2024-04-04 23:45:32 +0100

    connected to supabase successfully

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,27 +1,60 @@
-import { Button, Card, CardContent, Typography } from "@mui/material";
+import { Box, Button, Card, CardContent, CircularProgress, Typography } from "@mui/material";
 import { NavLink } from "react-router-dom";
 import Logo from './../assets/logo.png'
+import { useEffect, useState } from "react";
+import { supabase } from "../utils";
+import { UserType } from "../types/Types";
 
 
 export default function Main() {
 
-  let name = 'Mohanned Kadache'
-  let email = 'mohannedx1105@gmail.com'
+  const [users, setUsers] = useState<UserType[]>([]);
+
+  useEffect(() => {
+    const getUsers = async () => {
+
+      await new Promise(resolve => setTimeout(resolve, 1000))
+
+      const { data, error } = await supabase.from('User').select('*');
+      if (!error) {
+        setUsers(data || []);
+      } else {
+        console.log('error fetching data: ', error)
+      }
+    };
+
+    getUsers();
+  }, []);
 
   return (
-    <div className="flex justify-center items-center mt-80 sm:mx-0 md:mx-16 lg:mx-60 xl:mx-96">
-      <Card sx={{ p: 4 }}>
+    <div className="flex justify-center items-center mt-60 sm:mx-8 md:mx-8 lg:mx-60 xl:mx-96">
+      <Card sx={{ p: 4, width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
         <CardContent className="flex flex-col gap-2 justify-center items-center" >
           <img src={Logo} alt="logo" style={{ width: 60, height: 60 }} />
-          <Typography variant="inherit" fontSize={40} component="div">
-            {name}
-          </Typography>
-          <Typography variant="inherit" fontSize={22}>
-            {email}
-          </Typography>
-          <Typography variant="inherit" fontSize={18}>
-            You have been added to the waitlist!
-          </Typography>
+          {users.length > 0 ? (
+            <div>
+              {
+                users.map((user) => (
+                  <div key={user.id}>
+                    <Typography variant="inherit" fontSize={40} component="div" textAlign='center'>
+                      {user.name}
+                    </Typography>
+                    <Typography variant="inherit" fontSize={22} textAlign='center'>
+                      {user.email}
+                    </Typography>
+                  </div>
+                ))
+
+              }
+              <Typography variant="inherit" fontSize={18} marginTop={4} textAlign='center'>
+                You have been added to the waitlist!
+              </Typography>
+            </div>
+          ) : (
+            <Box sx={{ display: 'flex' }} margin={8}>
+              <CircularProgress sx={{color: '#14152C'}} />
+            </Box>
+          )}
 
           <NavLink to='/' className='mt-6'>
             <Button variant="contained" style={{ backgroundColor: "#C92A2A", fontFamily: 'Oswald' }} size="large"> Signout </Button>
