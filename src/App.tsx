e35b6d3381327d9f4406683dd81b938afc0336e8
muohannedxd <mohannedx1105@gmail.com>
2024-04-07 00:19:56 +0100

    all done

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,6 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import { Routes, Route } from 'react-router-dom';
-import AuthForms from './pages/AuthForms';
-import Main from './pages/Main';
-import NoMatch from './pages/NoMatch';
-import VerifyEmail from './pages/VerifyEmail';
-import Verified from './pages/Verified';
 
 export default function App() {
   return (
