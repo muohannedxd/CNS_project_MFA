d4a0b4d434f8a62595d94544fc965a979c2056c6
muohannedxd <mohannedx1105@gmail.com>
2024-04-07 00:23:13 +0100

    all done

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,23 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import { Routes, Route } from 'react-router-dom';
+import AuthForms from './pages/AuthForms';
+import Main from './pages/Main';
+import NoMatch from './pages/NoMatch';
+import VerifyEmail from './pages/VerifyEmail';
+import Verified from './pages/Verified';
 
 export default function App() {
   return (
     <div className="App">
-      <p color='white' style={{color: 'white'}}>Helloooo</p>
+      <Routes>
+        <Route path='/' element={<Main />}></Route>
+        <Route path='/authenticate' element={<AuthForms />}></Route>
+        <Route path='/verify-email' element={<VerifyEmail />}></Route>
+        <Route path='/verified' element={<Verified />} ></Route>
+        <Route path='*' element={<NoMatch />}></Route>
+      </Routes>
     </div>
   )
 }
